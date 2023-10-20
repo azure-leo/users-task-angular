561740889bfcd9a206976df5a840cc49a5d972ac
azure-leo <Felixculpa16>
2023-10-20 22:40:00 +0600

    Add footer

--- practice-app/apps/practice-app/src/app/app.config.ts
+++ practice-app/apps/practice-app/src/app/app.config.ts
@@ -1,22 +1,21 @@
-import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
 import { appRoutes } from './app.routes';
-import {HttpClientModule} from "@angular/common/http";
-import {API_URL} from "@users/core/http";
-import { environment} from "../environments/environment";
+import { HttpClientModule } from '@angular/common/http';
+import { API_URL } from '@users/core/http';
+import { environment } from '../environments/environment.development';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
-    importProvidersFrom(
-      HttpClientModule
-    ),
+    importProvidersFrom(HttpClientModule),
     {
       provide: API_URL,
-      useValue: environment.api_url
-    }
+      useValue: environment.api_url,
+    },
   ],
 };
--- practice-app/libs/users/data-access/src/public-api.ts
+++ practice-app/libs/users/data-access/src/public-api.ts
@@ -1,10 +1,6 @@
 /*
  * Public API Surface of data-access
  */
-
-export * from './lib/data-access.service';
-export * from './lib/data-access.component';
-export * from './lib/data-access.module';
 export * from './lib/user.interface'
 export * from './lib/UsersApiService.service'
 
