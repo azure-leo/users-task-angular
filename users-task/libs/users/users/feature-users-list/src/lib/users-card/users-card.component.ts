651cdc8dad01a4e1df1193f89ac87d1d18eccf5f
azure-leo <Felixculpa16>
2023-10-29 14:31:41 +0600

    feat(users card) add users card

---
+++ users-task/libs/users/users/feature-users-list/src/lib/users-card/users-card.component.ts
@@ -0,0 +1,24 @@
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {UsersVM} from "../users-vm";
+import {TuiIslandModule} from "@taiga-ui/kit";
+import {TuiButtonModule} from "@taiga-ui/core";
+
+@Component({
+  selector: 'users-task-users-card',
+  standalone: true,
+  imports: [CommonModule, TuiIslandModule, TuiButtonModule],
+  templateUrl: './users-card.component.html',
+  styleUrls: ['./users-card.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UsersCardComponent {
+  @Input({required: true})
+  user!: UsersVM;
+
+  @Output() deleteUser = new EventEmitter()
+
+  onDeleteUser(event: Event) {
+    this.deleteUser.emit()
+  }
+}
