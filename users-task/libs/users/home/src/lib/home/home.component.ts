65f99ca7a5f3c4e6c1c298ec2c382329da147f3f
azure-leo <Felixculpa16>
2023-10-30 03:08:53 +0600

    feat(home) add user add button

--- users-task/libs/users/home/src/lib/home/home.component.ts
+++ users-task/libs/users/home/src/lib/home/home.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersListContainerComponent } from '@users/feature-users-list';
+import {CreateUserButtonComponent} from "@users/feature-users-create";
 
 @Component({
   selector: 'users-task-home',
   standalone: true,
-  imports: [CommonModule, UsersListContainerComponent],
+  imports: [CommonModule, UsersListContainerComponent, CreateUserButtonComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   encapsulation: ViewEncapsulation.Emulated,
